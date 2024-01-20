1d38242c6ed99ba0c5bfe4176b84baa24df72a54
BenStokes75 <135236953+BenStokes75@users.noreply.github.com>
2024-01-20 11:15:06 +0000

    mainnn

---
+++ Java/Input_Length.java
@@ -0,0 +1,12 @@
+// Write a program that takes a string as input and prints its length to the console.
+package Java;
+
+public class Input_Length {
+    public static void main(String[] args) {
+        String str = "Enter the string";
+        System.out.println(str.length());
+        int len = str.length();
+        System.out.println("The length of the string is " + len);
+
+    }
+}
