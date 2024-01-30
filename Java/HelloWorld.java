cfa0350a93ef53ae96f14967972d6c115feabcfa
BenStokes75 <135236953+BenStokes75@users.noreply.github.com>
2024-01-30 09:39:40 +0000

    updated java file

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -3,6 +3,6 @@ package Java;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(" Jay Shree Ram");
     }
 }
