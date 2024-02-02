0fd8846f4d615e4101cf9eb0c1d48034ff50d374
BenStokes75 <135236953+BenStokes75@users.noreply.github.com>
2024-02-02 12:08:01 +0000

    updated java file

---
+++ Java/hello.java
@@ -0,0 +1,21 @@
+/* print jay shree ram */
+
+// package Java;
+// public class hello{
+//     public static void main(String[] args) {
+//         System.err.println("jay shree ram");
+//     }
+// }
+
+/* 𝗔𝗱𝗱𝗶𝗻𝗴 𝘁𝘄𝗼 𝗻𝘂𝗺𝗯𝗲𝗿𝘀 */
+
+package Java;
+public class hello{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.print(c);
+        System.out.println(" Varun");
+    }
+}
