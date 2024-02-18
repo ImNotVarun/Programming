ab1017c52cfad4763aa563caebaa7b4bdaa421b7
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-02-18 12:30:00 +0000

    createdd factorial java program in two three go

---
+++ Project_File_Java/factorial.java
@@ -0,0 +1,18 @@
+package Project_File_Java;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        try(Scanner a = new Scanner(System.in)){
+            System.out.println("enter the number");
+            int n = a.nextInt();
+            int mul=1;
+            for(int i=n;i>0;i=i-1){
+                mul =mul*i;
+            }
+            System.out.println("Factorial of the number "+n+ " is " +mul);
+
+        }
+    }
+}
