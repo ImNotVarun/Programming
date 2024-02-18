cfd8aff61599cd2797f41367ace99fd98a7ddb23
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-02-18 13:53:29 +0000

    not study armstrong thing

---
+++ Project_File_Java/armstrong.java
@@ -0,0 +1,14 @@
+package Project_File_Java;
+import java.util.Scanner;
+public class armstrong {
+
+    public static void main(String[] args) {
+        try(Scanner a = new Scanner(System.in))
+        {
+            System.out.println("Enter the number");
+            int n = a.nextInt();
+
+            if (n)
+        }
+    }
+}
