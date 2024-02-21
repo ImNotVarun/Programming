b90920c49a26f93c130d8cfee69c9ec1de364237
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-02-21 08:23:17 +0000

    updated file_exist or not

--- Linux/fileexist.sh
+++ Linux/fileexist.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
-# #checking the file exist with the name
+                                        # #checking the file exist with the name
 # echo enter the file name
 # read File
 # if [ -e $File ]
 # then echo 'file exist'
 # else echo 'file does not exist'
 # fi
-# checking that file exist with path and file name
+
+
+                                       # checking that file exist with path and file name
 
 echo enter the path or file name
 read file
 read path
 LOC="$path // $file"
 if [ -d $path ]
-then LOC  "$path // $file "
-if [ -e $LOC ]
-then echo "file exist"
-else echo "file does not exist"
-fi
-else echo "path does not exist"
+then
+  LOC="$path/$file"
+  if [ -e $LOC ]
+  then
+    echo "file exist"
+  else
+    echo "file does not exist"
+  fi
+else
+  echo "path does not exist"
 fi
 
-#arrays shell scripting
+                                            #arrays shell scripting
 
 # fruits=( orange mango apple )
 # fruits[0]=orange
