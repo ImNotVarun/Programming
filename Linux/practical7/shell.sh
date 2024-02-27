cd09962b672ebb03b3bbeb18df6587b564a8c52f
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-02-27 15:02:54 +0530

    updated the linux practical

--- Linux/practical7/shell.sh
+++ Linux/practical7/shell.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # print directory content if directory or print content of the file if file
-for i in *; do
-    if [ -d $i ]; then
-        ls "$i"
-        break
-    else
-        cat $i
-    fi
-done
+echo "Enter the path or file name"
+read input
+if [ -f "$input" ]; then
+    echo "The file content is : "
+    cat $input
+else
+    echo "The directory content is : "
+    ls "$input"
+fi
