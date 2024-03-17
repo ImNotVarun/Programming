06988c64474fe413424d8b87789a7e87952bfb9b
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-03-17 16:37:04 +0530

    student management started

---
+++ Python_realworld/studentmanagement.py
@@ -0,0 +1,9 @@
+# Student Grade Management In an educational system, a list can be used to store the grades of students in a class. Each element in the list can represent the grade of a particular student. Operations like calculating the average grade, finding the highest or lowest grade, or sorting the grades can be performed using list methods and functions.
+
+class1 = ["Varun", "Babli", "Khushi", "Virender"]
+class2 = ["Vishal", "Rohan", "Abhishek", "Harsh", "jigyasu", "anshul"]
+class3 = ["Himanshu", "Rahul"]
+
+subjects = ["Maths", "Science", "English", "Sst", "Hindi", "PT"]
+
+Varun = [20, 30, 40, 50, 30, 50]
