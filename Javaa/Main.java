cabe844349d39afd4239225f2ded46b44fe4b046
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-03-19 13:11:51 +0000

    Added Java

---
+++ Javaa/Main.java
@@ -0,0 +1,66 @@
+package Javaa;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+@SuppressWarnings("unused")
+public class Main {
+    // public static void main(String[] args) {
+    // int x = 123; // declaration and assinement
+    // System.out.println("the value of x is :" + x);
+    // long y = 234235345343L;
+    // System.out.println("The value of y is :" + y);
+    // double z = 1233.34353;
+    // System.out.println(z);
+    // boolean a = true; // they only hold true and false
+    // System.out.println(a);
+    // char symbol = '#';
+    // System.out.println(symbol);
+    // String b = "hello world";
+    // System.out.println(b);
+
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // int a = 10;
+    // int b = 20;
+    // int temp;
+    // temp = a;
+    // a = b;
+    // b = temp;
+    // System.out.println(a);
+    // System.out.println(b);
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // try (Scanner a = new Scanner(System.in)) {
+    // System.out.println("what is your name");
+    // String name = a.nextLine();
+    // System.out.println("What is your age :");
+    // int age = a.nextInt();
+    // a.nextLine();
+    // System.out.println("What is your favorite food");
+    // String food = a.nextLine();
+
+    // System.out.println("You name is :" + name);
+    // System.out.println("You age is :" + age);
+    // System.out.println("You Favorite food is :" + food);
+    // }
+    // }
+    // }
+
+    // public static void main(String[] args) {
+    // int f = 10;
+    // f++;
+    // System.out.println(f);
+    // }
+    // }
+
+    // GUI application
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("enter your name ");
+
+    }
+}
