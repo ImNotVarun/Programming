067963b474c3c297c2aeb7c531e8482a3583a59d
Varun <135236953+BenStokes75@users.noreply.github.com>
2024-03-23 00:45:18 +0530

    learned python dictionary today

---
+++ Python_realworld/dic.py
@@ -0,0 +1,28 @@
+print("This is a test for dictionary")
+
+Food = {
+    "Main dish": "Roti",
+    "sabji": "Allu ki sabji",
+    "Desert": "Panni"
+}
+
+print(Food)
+
+
+# nested dictionary
+
+dic = {
+    "name": "samay",
+    "section": "A",
+    "subjects": {
+        "C": 20,
+        "maths": 8,
+        "python": 21
+    }
+}
+print(dic)  # print entire dictionary
+print(dic["subjects"])  # print the subjects name and marks
+# it will print the marks in the python subject
+print(dic["subjects"]["python"])
+dic["section"] = "B"
+print(dic)
