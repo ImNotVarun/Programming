e335fe3ea1e3ebbd6c38ec672f7c4ff1bb44a593
BenStokes75 <BSTOKES4@art.edu>
2024-03-23 20:37:18 +0530

    _

--- Javaa/Main.java
+++ Javaa/Main.java
@@ -93,7 +93,7 @@ public class Main {
     // }
 
     public static void main(String[] args) {
-        String day = "Pizza";
+        String day = "mon";
         switch (day) {
             case "sunday":
                 System.out.println("it is sunday");
