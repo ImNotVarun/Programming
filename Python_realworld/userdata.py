8f18bc0208c560fcb2d843d8cf6a069d8c1e771e
BenStokes75 <BSTOKES4@art.edu>
2024-03-24 15:31:04 +0530

    Database

--- Python_realworld/userdata.py
+++ Python_realworld/userdata.py
@@ -27,43 +27,64 @@ User_five = {
     "age": 50,
     "city": "New York"
 }
+users = [User_One, User_two, User_three, User_four, User_five]
 
-print("Welcome to the Database \n enter waht you wanna do \n Here is What you can do \n 1.Add in the database \n 2.Delete from the database \n 3.See details of a customer from the database")
+print("Welcome to the Database \n enter what you wanna do \n Here is What you can do \n 1.Add a new cutomer in the database \n 2.Delete or add customer in  the database \n 3.See details of a customer from the database")
 
 entered = input("Enter your choice")
 match entered:
     case "3":  # See details of a customer from the database
         print("Enter the name of the customer")
         name = input().lower()
-        if name == User_One["name"]:
-            print(User_One)
-        elif name == User_two["name"]:
-            print(User_two)
-        elif name == User_three["name"]:
-            print(User_three)
-        elif name == User_four["name"]:
-            print(User_four)
-        elif name == User_five["name"]:
-            print(User_five)
+        for user in users:
+            if name == user["name"]:
+                print(user)
+                break  # break the loop if the customer is found
         else:
-            print("No such customer")
+            print("Customer not found")
+
     case "2":
         print("Enter the name of the customer you want to delete")
         name = input().lower()
-        if name == User_One["name"]:
-            print(User_One)
-            print("What you want to Delete \n 1.Name \n 2.Age \n 3.City")
-            choice = input()
-            if choice == "1":
-                User_One["name"] = input("Enter the new name")
-                print(User_One)
-            elif choice == "2":
-                User_One["age"] = int(input("Enter the new age"))
-                print(User_One)
-            elif choice == "3":
-                User_One["city"] = input("Enter the new city")
-                print(User_One)
-            else:
-                print("Invalid choice")
-        elif (name == User_two["name"]):
-            print(User_two)
+        for user in users:
+            if name == user["name"]:
+                print(user)
+                print(
+                    "What you want to Delete \n 1.Name \n 2.Age \n 3.City \n 4.Delete the customer")
+                choice = input()
+
+                if choice == "1":
+                    user["name"] = input("Enter the new name")
+                    print(user)
+
+                elif choice == "2":
+                    # Check if the input is an integer
+                    user["age"] = input("Enter the new age")
+                    if (user["age"].isdigit()):
+                        print("Done", user)
+                    else:
+                        print("Invalid age")
+
+                elif choice == "3":
+                    user["city"] = input("Enter the new city")
+                    print(user)
+
+                elif choice == "4":
+                    print("Customer deleted")
+                    users.remove(user)
+                    print(users)
+
+                else:
+                    print("Invalid choice")
+                break
+        else:
+            print("No such customer")
+    case "1":
+        print("Enter the name of the customer")
+        name = input().lower()
+        for user in users:
+            if name == user["name"]:
+                print("Customer already exists")
+                break
+        else:
+            print("Customer not found")
