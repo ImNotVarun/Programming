0dadacd984c66b86676cc454142c0b8431c468ed
BenStokes75 <BSTOKES4@art.edu>
2024-03-25 23:10:13 +0530

    invoices

---
+++ Python_realworld/Invoices/invoices.py
@@ -0,0 +1,8 @@
+# A company wants to automate the process of sending out invoices to customers. They have a list of customer data stored in a CSV file. Write a Python function that takes the CSV file as input and generates a PDF invoice for each customer.
+
+import csv
+
+with open('D:\Compiling_Coding\Programming\Python_realworld\Invoices\customers.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
