f7c2c3b24ac530e803cca6d99983c3a146d718ca
BenStokes75 <BSTOKES4@art.edu>
2024-04-11 17:48:43 +0530

    user input

--- Javascript/script.js
+++ Javascript/script.js
@@ -11,7 +11,7 @@ console.log("This is a script file.");
 
 // This is a comment.
 
-/* This is a multi-line 
+/* This is a multi-line
 comment. */
 
 //? for changing the text content of an element
@@ -41,4 +41,28 @@ console.log(`Your Name is ${name}\nYou are ${age} old\nYou are from ${city} \nYo
 let name1 = true
 if (name1 = true) {
     console.log(`Hello ${name}`)
+}
+
+//? example
+document.getElementById("myh1").textContent = `Hello ,${name}`;
+document.getElementById("myp").textContent = `My Name is ${name} and i am ${age} old , I am from ${city} and i like to eat ${food} , if you want to contact me please dia anything on this mail ${email}`
+
+
+//! accepting User Input
+
+//? Easy way using window prompt
+
+let Username = window.prompt("whar is Your Username");
+
+document.getElementById("username").textContent = `Hello ,${Username}`;
+document.getElementById("usernamep").textContent = `My Name is ${Username}`;
+
+//? Professional Way to do it 
+
+let myname;
+
+document.getElementById("button").onclick = function () {
+    myname = document.getElementById("mynamee").value;
+    document.getElementById("user").textContent = `Hello ,${myname}`;
+    document.getElementById("userr").textContent = `My Name is ${myname}`;
 }
