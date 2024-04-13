fc848612e89c906a897da5ea5ccd039a15ddfcc8
BenStokes75 <BSTOKES4@art.edu>
2024-04-13 23:06:41 +0530

    updated for much smoother experience

--- Javascript/18+consentpage/index.js
+++ Javascript/18+consentpage/index.js
@@ -1,20 +1,24 @@
-document.getElementById("consentbtn1").onclick = function () {
+const consentbtn1 = document.getElementById("consentbtn1")
+const consentbtn2 = document.getElementById("consentbtn2")
+const lol = document.getElementById("lol")
+
+consentbtn1.onclick = function () {
     const input1 = Number(window.prompt("Give me Proof that you are a Junior Developer , what is 2+2="));
     if (input1 == 4) {
-        document.getElementById("lol").textContent = "You can Proceed Now"
+        lol.textContent = "You can Proceed Now"
     }
     else {
-        document.getElementById("lol").textContent = "You Liar"
+        lol.textContent = "You Liar"
     }
 }
 
-document.getElementById("consentbtn2").onclick = function () {
+consentbtn2.onclick = function () {
     const input2 = Number(window.prompt("Give me Proof that you are a Expert Developer , what is 2+2="));
     if (input2 == 4) {
-        document.getElementById("lol").textContent = "You can Proceed Now"
+        lol.textContent = "You can Proceed Now"
     }
     else {
-        document.getElementById("lol").textContent = "You Liar"
+        lol.textContent = "You Liar"
     }
 
 }
