6aa97a6b69aa8edeb501a997836de65eb38c6e14
BenStokes75 <BSTOKES4@art.edu>
2024-04-16 16:36:36 +0530

    updated for smaller screens and modifyied a little bit

--- Javascript/Newsletter/index.js
+++ Javascript/Newsletter/index.js
@@ -24,6 +24,11 @@ submit.onclick = function () {
             success.textContent = `Thank you for signing up!`;
             para.textContent = `You will now receive our latest updates and we can collect telemenatry data. `;
         }
+        else if (box.checked) {
+            success.textContent = `Thank you for signing up!`;
+            para.textContent = `You will now receive our latest Promotions and updates and we can collect telemenatry data. `;
+
+        }
 
         else {
             success.textContent = `Getting error while Signing You In`;
