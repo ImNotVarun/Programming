97f852d9ed4a638ce9879c9720b9654ea3f20c60
BenStokes75 <BSTOKES4@art.edu>
2024-04-23 15:41:39 +0530

    updated

--- Javascript/Temprature_convertor/index.js
+++ Javascript/Temprature_convertor/index.js
@@ -7,11 +7,11 @@ const Result = document.getElementById('Result');
 submit.onclick = function () {
     if (celsiusToFahrenheit.checked) {
         let tem = (input.value * 9 / 5) + 32;
-        Result.textContent = `${tem.toFixed(2)}F`;
+        Result.textContent = `${tem.toFixed(2)}°F`;
     }
     else if (fahrenheitToCelsius.checked) {
         let tem = (input.value - 32) * 5 / 9;
-        Result.textContent = `${tem.toFixed(2)}C`;
+        Result.textContent = `${tem.toFixed(2)}°C`;
     }
     else {
         Result.textContent = " You didn't pass a value or not select the convertion type"
