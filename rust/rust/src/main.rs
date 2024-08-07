e1386f08342246084d4ebbd61884cd854d30a64c
BenStokes75 <BSTOKES4@art.edu>
2024-08-08 01:05:33 +0530

    updated

--- rust/rust/src/main.rs
+++ rust/rust/src/main.rs
@@ -1,9 +1,25 @@
+use std::io;
+
 fn main() {
     function1();
     function2();
-    function3();
+    // function3();
     adding_numbers(23, 34);
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Press x to exit the terminal");
+        let input = input.trim();
+        if input == "X" {
+            println!("Exiting the terminal");
+            break;
+        } else {
+            print!("You have entered wrong input {}", input);
+        }
+    }
 }
+
 fn function1() {
     let num: u8 = 255; // immuataable variable
     let mut num2: u8 = 255; // mutable variable
@@ -21,20 +37,20 @@ fn function2() {
     println!("{} - &str {}", string, string1);
 }
 
-fn function3() {
-    // tuple - can store multiple values of different types of data
-    let student: (&str, i8, &str) = ("Varun", 20, "Male");
-    println!("{:?}", student); // the :? is used for debugging purpouses
-    println!("{} {} {}", student.0, student.1, student.2); // .0 .1 .2 are used to access the values of the tuple
+// fn function3() {
+//     // tuple - can store multiple values of different types of data
+//     let student: (&str, i8, &str) = ("Varun", 20, "Male");
+//     println!("{:?}", student); // the :? is used for debugging purpouses
+//     println!("{} {} {}", student.0, student.1, student.2); // .0 .1 .2 are used to access the values of the tuple
+// }
+// // destructuring
 
-    // destructuring
-
-    let (name, age, gender) = student;
-    print!(
-        "through destructuring , name {} age {} Gender {}",
-        name, age, gender
-    );
-}
+//     let (name, age, gender) = student;
+//     print!(
+//         "through destructuring , name {} age {} Gender {}",
+//         name, age, gender
+//     );
+// }
 
 fn adding_numbers(num1: u8, num2: u8) {
     print!("{}", num1 + num2) // adding numbers
