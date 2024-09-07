ffbf8839daa241c9e481009b6651dbe61705da87
Varun <imnotvarun@gmail.com>
2024-09-07 10:03:05 +0000

    c++ baby

--- c++/code.cpp
+++ c++/code.cpp
@@ -2,7 +2,16 @@
 #include <unistd.h>
 using namespace std;
 
+int age_verification();
+int loops();
 int main()
+{
+    // age_verification();
+    loops();
+    return 0;
+}
+
+int age_verification()
 {
     int age;
     cout << "---------------Checking Are you 18+ or Not ----------------" << endl;
@@ -24,6 +33,36 @@ int main()
     {
         cout << "You have passed the test by cheating so you are not allowed to visit this adult website until your 18+" << endl;
     }
+    return 0;
+}
 
+int loops()
+{
+    int n = 5;
+    int count = 0;
+
+    // for loops for printing 1 to 5 numbers
+    for (int i = 0; i <= n; i++)
+    {
+        cout << "The Numbers are = " << i << endl;
+    }
+
+    // while loops for doing the same thing
+    while (n >= count)
+    {
+        cout << count << endl;
+        count++;
+    }
+
+    //? adding numbers from the nth position
+    int num1;
+    int sum = 0;
+    cout << "ENter the number" << endl;
+    cin >> num1;
+    for (int i = 0; i <= num1; i++)
+    {
+        sum = i + sum;
+    }
+    cout << sum << endl;
     return 0;
 }
