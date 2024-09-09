004237fc422acc77eb61b3d142a8efbc73eeb3a5
Varun <imnotvarun@gmail.com>
2024-09-09 11:32:06 +0000

    pattern c++ baby

--- c++/pattern.cpp
+++ c++/pattern.cpp
@@ -3,9 +3,15 @@
 #include <iostream>
 using namespace std;
 int Q1();
+int Q2();
+int Q3();
+int Q4();
 int main()
 {
-    Q1();
+    // Q1();
+    // Q2();
+    // Q3();
+    Q4();
     return 0;
 }
 
@@ -19,6 +25,72 @@ int Q1()
         for (int j = 1; j <= num; j++)
         {
             cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//! printing  sequentical order of number in different rows
+int Q2()
+{
+    int num1 = 1;
+    int rows;
+    cout << "Enter Rows" << endl;
+    cin >> rows;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            cout << num1;
+            num1++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/* printing
+0
+11
+222
+3333
+44444
+*/
+int Q3()
+{
+    int rows;
+    cout << "Enter the rows" << endl;
+    cin >> rows;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//! floyd's triangle printing
+
+int Q4()
+{
+    int rows;
+    cout << "Enter Rows" << endl;
+    cin >> rows;
+    int num1 = 1;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << num1;
+            num1++;
         }
         cout << endl;
     }
